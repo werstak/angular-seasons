7ebd06355a78162bafa839249551a05eb6580e9c
werstak <webproger1@gmail.com>
2021-02-11 17:11:23 +0200

    cgancged - MonthsService

---
+++ src/app/interfaces/month.ts
@@ -0,0 +1,8 @@
+export class Month {
+  name?: string;
+  image?: string;
+  date?: string;
+  description?: string;
+  daysCount?: number;
+  season?: string;
+}
--- src/app/services/months.service.ts
+++ src/app/services/months.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Month } from '../interfaces/month';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonthsService {
 
-  constructor() { }
+  constructor(private readonly httpClient: HttpClient) { }
+
+
+  getItemAMonth(): Observable<Month> {
+    return this.httpClient.get<Month>('http://localhost:8080/')
+      .pipe(
+        catchError(error => {
+          console.log('Error: ', error.message);
+          return throwError(error);
+        }),
+      );
+  }
 }
+
